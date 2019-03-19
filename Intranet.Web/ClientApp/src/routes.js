2a328db1ba9b3534daa3e874caaa5ade247588a2
Mirosław <miroslaw.sawczuk@gmail.com>
2019-03-19 13:49:19 +0100

    Added PageNotFound template

--- Intranet.Web/ClientApp/src/routes.js
+++ Intranet.Web/ClientApp/src/routes.js
@@ -2,6 +2,8 @@ import HomePage from './views/HomePage.vue';
 import PostLogin from './views/PostLogin.vue';
 import User from './views/User.vue';
 import FirstConfiguration from './views/FirstConfiguration.vue';
+import PageNotFound from './views/Errors/PageNotFound.vue';
+
 
 export default [
   { 
@@ -23,5 +25,10 @@ export default [
     name: 'firstconfiguration',
     path: '/first-configuration',
     component: FirstConfiguration
+  },
+  { 
+    name: 'pagenotfound',
+    path: '/page-not-found',
+    component: PageNotFound
   }
 ]
---
+++ Intranet.Web/ClientApp/src/views/Errors/PageNotFound.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <span class='text-block'>Page Not Found</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PageNotFound',
+}
+</script>
+
+<style lang="scss">
+  .text-block{
+    font-size: 5rem;
+    padding: 1em;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    margin-right: -50%;
+    transform: translate(-50%, -50%);
+  }
+</style>
