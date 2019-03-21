9a957b9cfdc4fcff83480795b0e1476cf2f1259f
Mirosław <miroslaw.sawczuk@gmail.com>
2019-03-21 18:25:39 +0100

    Removed unnecessary import

--- Intranet.Web/ClientApp/src/App.vue
+++ Intranet.Web/ClientApp/src/App.vue
@@ -8,15 +8,11 @@
 <script>
 import bootstrap from 'bootstrap/dist/js/bootstrap.js';
 import Header from './components/Header.vue';
-import User from './views/User.vue';
-import FirstConfiguration from './views/FirstConfiguration.vue';
 
 export default {
   name: 'app',
   components: {
     Header,
-    User,
-    FirstConfiguration
   }
 }
 </script>
